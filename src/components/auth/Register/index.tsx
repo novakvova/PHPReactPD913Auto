ddf2f824652c46bc47d4f8ce414f00bd2768083a
novakvova <novakvova@gmail.com>
2021-12-17 16:48:35 +0200

    Upload image is good cropper

--- src/components/auth/Register/index.tsx
+++ src/components/auth/Register/index.tsx
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import {Modal} from 'bootstrap';
+import Cropper from 'cropperjs';
 import InputGroup from "../../common/InputGroup";
 
 interface IRegisterPage {
@@ -8,20 +10,37 @@ interface IRegisterPage {
 
 const RegisterPage = () => {
 
+  let modal: Modal;
+  let cropper: Cropper;
+  const modalRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
+  
   const [model, setModel] = useState<IRegisterPage>({} as IRegisterPage);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    
-    setModel({
-      ...model,
-      [e.target.name]: e.target.value
+
+  useEffect(() => {
+    modal = new Modal(modalRef.current as Element,{
+      backdrop: "static",
+      keyboard: false
     });
-  }
+  }, [])
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("submit form", model);
-    
+    modal.show(); 
+
+    cropper = new Cropper(imageRef.current as HTMLImageElement, {
+      aspectRatio: 1 / 1,
+      crop(event) {
+        console.log(event.detail.x);
+        console.log(event.detail.y);
+        console.log(event.detail.width);
+        console.log(event.detail.height);
+        console.log(event.detail.rotate);
+        console.log(event.detail.scaleX);
+        console.log(event.detail.scaleY);
+      },
+    }); 
   }
 
   return (
@@ -50,6 +69,46 @@ const RegisterPage = () => {
               Реєстрація
             </button>
           </form>
+        </div>
+      </div>
+
+      <div className="modal" ref={modalRef} tabIndex={-1}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p>Modal body text goes here.</p>
+              <div>
+                <img ref={imageRef} style={{maxWidth: "100%", display: "block"}} 
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT48ukN18yyXjwJu00yMd3gd4qVi5sBmOgRLg&usqp=CAU" 
+                  alt="girl" />
+              </div>
+
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary"
+                onClick={() =>{
+                  cropper.rotate(90);
+                }}>
+                Save changes
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
